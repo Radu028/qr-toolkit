5fa0e04332c086706b337065df261da32cb15fca
Radu <radupopa028@gmail.com>
2025-01-23 21:26:54 +0200

    Add binary thresholding to image processing

--- main.py
+++ main.py
@@ -7,3 +7,6 @@ height, width = img.shape
 print(height, width)
 
 print(img[100, 100])  # intensity of pixel (row=100, col=100)
+
+_, binary_img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+# Now binary_img is either 0 (black) or 255 (white)
